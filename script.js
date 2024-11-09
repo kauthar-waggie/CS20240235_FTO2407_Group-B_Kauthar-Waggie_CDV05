f6eb9d5eea41549db0d1b775d3c952a55ab2ff56
Kauthar Waggie <kautharwaggie1@gmail.com>
2024-11-09 17:10:03 +0200

    still minor adjustments to fix

---
+++ script.js
@@ -0,0 +1,44 @@
+function toggleMenu() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("show");
+}
+
+// Close the menu if the user clicks outside the navbar or on the menu button
+document.addEventListener('click', function(event) {
+  const navbar = document.getElementById('navbar');
+  const menuToggle = document.querySelector('.menu-toggle');
+
+  // If the click is outside the navbar and not on the toggle button, close the menu
+  if (!navbar.contains(event.target) && !menuToggle.contains(event.target)) {
+      navbar.classList.remove('show');
+  }
+});
+
+
+function toggleDetails(card) {
+  // Get all project cards
+  const allCards = document.querySelectorAll('.project-card');
+
+  // Remove the 'expanded' class from all cards
+  allCards.forEach(function(item) {
+      if (item !== card) {
+          item.classList.remove('expanded');
+      }
+  });
+
+  // Toggle the 'expanded' class on the clicked card
+  card.classList.toggle('expanded');
+}
+
+
+
+window.addEventListener("scroll", function() {
+  const projectsTitle = document.querySelector(".slide-in");
+  const position = projectsTitle.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (position < windowHeight - 50) {
+      projectsTitle.style.animation = "slideIn 2s ease-out forwards";
+  }
+});
+
